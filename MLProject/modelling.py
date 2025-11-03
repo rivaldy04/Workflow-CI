846e42849b9d67aef0dcad1efa3661e9b73aef7d
rivaldy04 <161176924+rivaldy04@users.noreply.github.com>
2025-11-03 22:19:50 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 import random
 import numpy as np
 
-mlflow.set_tracking_uri("http://127.0.0.1:5000/")
+# mlflow.set_tracking_uri("http://127.0.0.1:5000/")
 mlflow.set_experiment("Latihan MLFlow Auto Logging v2")
 
 mlflow.sklearn.autolog(
